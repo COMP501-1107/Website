6dd55500b1e1722afdfa47e72e19570229ac1932
Nate E <106148472+natevaut@users.noreply.github.com>
2022-06-07 11:45:32 +1200

    Update nav
    Shrink the non-content page links

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
 	document.querySelector('nav').innerHTML = `
 		<ul>
 			<li><a href="${ROOT}/index.html">Home page</a></li>
-		</ul><ul>
+		</ul>
+		<ul>
 			<li><a href="${ROOT}/misc/topic.html">Technology/Topic</a></li>
-		</ul><ul>
+		</ul>
+		<ul>
 			<li><a href="${ROOT}/misc/opportunities.html">Opportunities</a></li>
 			<li><a href="${ROOT}/misc/risks.html">Risks</a></li>
 			<li><a href="${ROOT}/misc/choices.html">Choices</a></li>
-		</ul><ul>
+		</ul>
+		<ul>
 			<li><a href="${ROOT}/misc/references.html">References</a></li>
-		</ul><ul>
+		</ul>
+		<ul style="font-size: 60%;>
 			<li><a href="${ROOT}/misc/ethics.html">Ethical Reflections</a></li>
 			<li><a href="${ROOT}/misc/process.html">Process Support</a></li>
 			<li><a href="${ROOT}/misc/team-formation.html">Team Formation</a></li>
